ecff82ae16cbac9209ff6d08b1a1df5ed449e044
VaheAbelyan <abelyan.vahe@gmail.com>
2023-03-03 09:53:50 +0400

    improvments

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-const favs = [];
+const favs = window.localStorage.getItem("favs")
+  ? JSON.parse(window.localStorage.getItem("favs"))
+  : [];
+
+const renderFavs = () => {
+  document
+    .querySelectorAll(".items .list .item .image-wrapper i")
+    .forEach((fav) => {
+      const { id } = fav.dataset;
+      if (favs.includes(parseInt(id))) {
+        fav.classList.remove("fa-regular");
+        fav.classList.add("fa-solid");
+      } else {
+        fav.classList.remove("fa-solid");
+        fav.classList.add("fa-regular");
+      }
+    });
+  document.querySelector("header .favourites .heart span").innerHTML =
+    favs.length;
+};
+
 window.onload = (event) => {
+  renderFavs();
   // Fav Add/Delete functionality
   document
-    .querySelectorAll("#cats .list .cat .image-wrapper i")
+    .querySelectorAll(".items .list .item .image-wrapper i")
     .forEach((fav) =>
       fav.addEventListener("click", (event) => {
         event.preventDefault(); // don't navigate away
-        var id = event.target.dataset.id;
-        if (favs.includes(id)) {
+        const { id } = event.target.dataset;
+        if (favs.includes(parseInt(id))) {
           favs.splice(favs.indexOf(id), 1);
-          event.target.classList.remove("fa-solid");
-          event.target.classList.add("fa-regular");
         } else {
-          favs.push(id);
-          event.target.classList.remove("fa-regular");
-          event.target.classList.add("fa-solid");
+          favs.push(parseInt(id));
         }
-        document.querySelector("header .favourites .heart span").innerHTML =
-          favs.length;
+        window.localStorage.setItem("favs", JSON.stringify(favs));
+        renderFavs();
       })
     );
 
   // Reset Button functionality
   document
-    .querySelector("#cats .toolbar .reset")
+    .querySelector(".items .toolbar .reset")
     .addEventListener("click", (event) => {
-      document
-        .querySelectorAll("#cats .list .cat .image-wrapper i")
-        .forEach((fav) => {
-          fav.classList.remove("fa-solid");
-          fav.classList.add("fa-regular");
-        });
       favs.splice(0, favs.length);
-      document.querySelector("header .favourites .heart span").innerHTML =
-        favs.length;
+      window.localStorage.setItem("favs", JSON.stringify(favs));
+      renderFavs();
     });
 };
